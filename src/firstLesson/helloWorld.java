e89f57bc2e8762b68fe38441c338fb02883c37eb
Kirill <hotasice41@gmail.com>
2020-08-29 17:21:48 +0300

    Changed Moscow to Minsk

--- src/firstLesson/helloWorld.java
+++ src/firstLesson/helloWorld.java
@@ -2,6 +2,6 @@ package firstLesson;
 
 public class helloWorld {
     public static void main(String[] args){
-        System.out.println("Hello, Moscow!");
+        System.out.println("Hello, Minsk!");
     }
 }
